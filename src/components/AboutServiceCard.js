8c4315e7b5c8568600c596d9c82bdc1357217793
SameerBramhecha14 <bramhechas89@gmail.com>
2024-04-04 00:41:50 +0530

    added download file feature

--- src/components/AboutServiceCard.js
+++ src/components/AboutServiceCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../assets/css/AboutServiceCard.css';
 import { useAuth } from '../context/AuthContext';
 import axios from 'axios';
@@ -7,8 +7,7 @@ import toast,{Toaster} from 'react-hot-toast';
 const AboutServiceCard = (props) => {
 
   const { user } = useAuth();
-  const { heading1} = props;
-
+  const { heading1 } = props;
   const handlePayment = async() => {
     try {
       console.log(props);
@@ -16,7 +15,12 @@ const AboutServiceCard = (props) => {
       const response = await axios.post('http://localhost:4000/pay', { amount: 1, service_name : heading1, payer: user.account });
       console.log(response);
       if (response.data.status === 200) {
+        console.log(response.data.data);
+        const hash = response.data.data;
         toast.success(response.data.message);
+        const openLink = `http://127.0.0.1:8081/ipfs/${hash}`;
+        console.log(openLink);
+        window.open(openLink);
       } else {
         toast.error("Payment Unsuccessful");
       }
