48645e1dbd6a666bd5e7ea4d14840cfb28b5e068
Aditi Chavan <chavanaditigulab@gmail.com>
2024-04-04 12:18:50 +0530

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,24 @@
 import React from 'react';
-import '../assets/css/Footer.css'; // Import your CSS file for styling if needed
-import { Link } from 'react-router-dom';
-
+import '../assets/css/Footer.css';
+// import { icon, logo } from './assets';
+// import { FaInstagram,FaDribbble,FaXTwitter,FaYoutube } from "react-icons/fa6";
 
 const Footer = () => {
+
     return (
-        <footer className="footer">
-            <div className="footerContent">
-                <div className="footerSection">
-                    <h3>About Us</h3>
-                    <p>We are a team of ambitious final-year CE undergraduate students from PICT College, Pune, driven by our passion for blockchain technology and our desire to make a meaningful impact. As blockchain enthusiasts, we are committed to exploring the potential of this revolutionary technology to address real-life problems and drive positive change in society. With a strong foundation in development and a keen interest in innovation, we are constantly pushing the boundaries to create innovative solutions that have the power to transform industries and improve lives. Our journey is fueled by our collective drive to learn, grow, and make a difference, and we are excited to present you AI Marketplace as we work towards building a brighter future through blockchain innovation.</p>
-                </div>
-
-                <div className="footerSection">
-                    <h3>Quick Links</h3>
-                    <ul>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to="/aboutus">About Us</Link></li>
-                        <li><Link to="/aimarketplace">AI MarketPlace</Link></li>
-                        <li><Link to="/signup">Sign Up</Link></li>
-                        <li><Link to="/signin">Sign In</Link></li>
-                    </ul>
-                </div>
-            </div>
-
-            <div className="footerBottom">
-                <p>&copy; 2024 All rights reserved.</p>
-            </div>
+        <div class="footer-basic">
+        <footer>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">Services</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">AI MarketPlace © 2024</p>
         </footer>
-    );
+        </div>
+    )
 }
 
-export default Footer;
+export default Footer
