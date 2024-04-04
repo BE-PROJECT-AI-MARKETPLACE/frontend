0a5a2c57d89c28ef818195828203c649f8c48b10
Aditi Chavan <chavanaditigulab@gmail.com>
2024-04-04 10:51:04 +0530

    Home Page Content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import AboutContent from '../components/AboutContent';
-import Vision from '../components/Vision';
-
+import 'animate.css';
+import '../assets/css/AboutContent.css';
 const Home = () => {
-    
-    
     return (
-        <div>
-            <AboutContent />
-            <Vision />
-                
+    <div class="container">
+    <div class="text-white">
+        <div class="py-4">
+            <h1 class="display-4 mb-4">AI MarketPlace</h1>
+            <p class="lead">Whether you're a startup looking to optimize operations or a seasoned enterprise aiming to stay ahead of the curve, our AI MarketPlace caters to the diverse needs of users across industries.</p>
+            <p class="lead py-4">Seamlessly navigate through a diverse range of AI Models tailored to address your specific business challenges.</p>
         </div>
-    )
+    
+        <div class="py-4">
+            <h1 class="display-4 mb-4">Our Vision</h1>
+            <p class="lead">To empower businesses of all sizes and industries with secure, transparent, and easy access to AI models. Our goal is to foster collaboration, innovation, and positive change, ultimately enabling businesses to thrive in the digital age.</p>
+            <p class="lead py-5">By leveraging blockchain technology, we ensure trust and integrity in the AI ecosystem.</p>
+        </div>
+
+        <div class="py-4">
+            <h2 class="display-4" >Join us as we pave the way towards a smarter, more connected future powered by AI.</h2>
+        </div>
+        </div>
+    </div>
+    );
 }
 
 export default Home;
